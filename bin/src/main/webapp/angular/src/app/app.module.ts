200e18f46d1e4e48efc9a8ddf07cc80ea0b68393
Joshua Bjork <joshuabjork@gmail.com>
2019-02-14 10:21:47 -0500

    adding angular login

---
+++ bin/src/main/webapp/angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginService } from './services/login-service/login-service.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+
+  ],
+  providers: [LoginService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ bin/src/main/webapp/angular/src/app/services/login-service/login-service.component.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LoginService {
+  constructor(private http: HttpClient) { }
+
+  getUser(): Observable<User>{
+    return this.http.get<User>("--insert URL to GET user from servlet--");
+  }
+}
+
+export interface User{
+  id: number,
+  email: string,
+  username: string,
+  firstname: string,
+  lastname: string
+  //might not need all of these for User object
+}
