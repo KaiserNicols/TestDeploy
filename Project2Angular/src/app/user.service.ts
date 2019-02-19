c805e0bb41ba6525c17f6d2b5314adb1beca9af7
Joshua Bjork <joshuabjork@gmail.com>
2019-02-19 15:10:23 -0500

    please don't break

--- Project2Angular/src/app/user.service.ts
+++ Project2Angular/src/app/user.service.ts
@@ -2,18 +2,13 @@ import { Injectable, Input } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-/*
+
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
     'Authorization': 'my-auth-token'
   })
 };
-<<<<<<< HEAD
-=======
-*/
-
->>>>>>> 376ebaa769da514437fdbd9b194ccee239788514
 
 @Injectable({
   providedIn: 'root',
@@ -21,24 +16,13 @@ const httpOptions = {
 
 @Injectable()
 export class UserService {
-<<<<<<< HEAD
-=======
-  constructor(private http: HttpClient) { }
-  //private baseUrl: string = 'http://localhost:8080/Project2/rest/';
-   private baseUrl: string = 'http://54.145.242.129:8080/Project2/rest/'
->>>>>>> 376ebaa769da514437fdbd9b194ccee239788514
 
   constructor(private http: HttpClient) { }
   private baseUrl: string = 'http://localhost:8080/Project2/rest/';
   //private baseUrl: string = 'http://54.145.242.129:8080/Project2/rest/'
 
-<<<<<<< HEAD
   loginUser(userAttempt: User): Observable<User> {
     return this.http.post<User>(this.baseUrl + "user", userAttempt, httpOptions);
-=======
-  loginUser(userAttempt: UserAttempt): Observable<UserAttempt> {
-    return this.http.post<User>(this.baseUrl + "user", userAttempt);
->>>>>>> 376ebaa769da514437fdbd9b194ccee239788514
   }
 }
 
