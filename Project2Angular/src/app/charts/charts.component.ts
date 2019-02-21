296a515748fe9f3c4ef617489312656c0527397b
jasonwdavies <jasonwdaviesart@gmail.com>
2019-02-21 10:59:23 -0500

    removing first chart

--- Project2Angular/src/app/charts/charts.component.ts
+++ Project2Angular/src/app/charts/charts.component.ts
@@ -33,7 +33,7 @@ export class ChartsComponent implements OnInit {
   constructor(private recommendationService: RecommendationService) {}
 
   ngOnInit() {
-    this.getRecommendations();
+    //this.getRecommendations();
     this.getAllRecommendations();
   }
 
