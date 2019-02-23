7589ef0da76233af57f3f8cd7273d3b4a52366f9
Joshua Bjork <joshuabjork@gmail.com>
2019-02-22 22:31:48 -0500

    working on recommendations

--- Project2Angular/src/app/new-rec/new-rec.component.ts
+++ Project2Angular/src/app/new-rec/new-rec.component.ts
@@ -51,6 +51,7 @@ export class NewRecComponent implements OnInit {
     this.submitResponse.username = this.user.username;
     this.submitResponse.movieId = this.recId;
     this.submitResponse.helpful = 1;
+    console.log(this.submitResponse);
     this.recsService.submitFeedback(this.submitResponse).subscribe(
       data => {
         console.log(data);
@@ -63,6 +64,7 @@ export class NewRecComponent implements OnInit {
     this.submitResponse.username = this.user.username;
     this.submitResponse.movieId = this.recId;
     this.submitResponse.helpful = 0;
+    console.log(this.submitResponse);
     this.recsService.submitFeedback(this.submitResponse).subscribe(
       data => {
         console.log(data);
@@ -90,32 +92,38 @@ export class NewRecComponent implements OnInit {
     * &release_date.lte=123 - release date less than 123
     * &vote_average.gte=123 - vote average greater than 123
     * &vote_average.lte=123 - vote average less than 123
+    * &with_cast=123 - with actor #123
     * &with_genres=123 - with genre #123
-    * &with_people=123 - with actor #123
+    * &year=123 - year #123
     */
-    // if (this.release!=null){
-    //   this.getAppend = this.getAppend + "&release_date.gte=" + this.release;
-    // }
-    // if (this.rating!=null){
-    //   this.getAppend = this.getAppend + "&vote_average.gte=" + this.rating;
-    // }
+    if (this.releaseGreater!=null){
+      this.getAppend = this.getAppend + "&release_date.gte=" + this.releaseGreater;
+    }    
+    if (this.releaseLower!=null){
+      this.getAppend = this.getAppend + "&release_date.lte=" + this.releaseLower;
+    }
+    if (this.ratingGreater!=null){
+      this.getAppend = this.getAppend + "&vote_average.gte=" + this.ratingGreater;
+    }
+    if (this.ratingLower!=null){
+      this.getAppend = this.getAppend + "&vote_average.gte=" + this.ratingLower;
+    }
+    if (this.selectActor!=null){
+      this.recsService.getActor(this.selectActor).subscribe(
+        data => { console.log(data);
+        this.tempData = data["results"];
+        this.tempData = this.tempData[0];
+        this.actorId = this.tempData["id"];
+        this.getAppend = this.getAppend + "&with_people=" + this.actorId;
+        }
+        ,err => console.log(`Error: ${err}`)
+      )}
     if (this.selectGenre!=null){
       for (let i = 0; i < this.genres.length; i++)
         if (this.selectGenre === this.genres[i].name){
           this.getAppend = this.getAppend + "&with_genres=" + this.genres[i].id}
     }
-    // if (this.selectActor!=null){
-    //   this.recsService.getActor(this.selectActor).subscribe(
-    //     data => { console.log(data);
-    //     this.tempData = data["results"];
-    //     this.tempData = this.tempData[0];
-    //     this.actorId = this.tempData["id"];
-    //     this.getAppend = this.getAppend + "&with_cast=" + this.actorId;
-    //     }
-    //     ,err => console.log(`Error: ${err}`)
-    //   )}
-
-    //director: "&with_cast=" + this.directorId;
+    console.log(this.getAppend);
     console.log(this.getAppend);
     this.recsService.getReccomendation(this.getAppend).subscribe(
       data => {this.recommendation = data["results"];
@@ -124,7 +132,6 @@ export class NewRecComponent implements OnInit {
                 this.tempData = this.recommendation[Math.floor(Math.random() * this.recommendation.length+1)]
                 this.recTitle = this.tempData["original_title"];
                 this.recId = this.tempData["id"];
-                // this.recPosterURL ="http://image.tmdb.org/t/p/w185" + this.tempData["poster_path"];
                 this.recPosterURL ="http://image.tmdb.org/t/p/w342" + this.tempData["poster_path"];
                 console.log(this.recPosterURL);
                 if (this.recTitle = undefined){
