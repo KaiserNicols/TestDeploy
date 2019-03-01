2cabe7f6c94779940904bd0bc16855838c6d01d3
Kale Harrison <kalecurtisharrison@gmail.com>
2019-02-28 20:19:40 -0500

    EC2 ip address changed

--- Project2Angular/src/app/recommendation.service.ts
+++ Project2Angular/src/app/recommendation.service.ts
@@ -13,7 +13,7 @@ const httpOptions = {
 })
 export class RecommendationService {
 
- private baseUrl: string = 'http://54.227.28.142:8080/Project2/rest/rec/'
+ private baseUrl: string = 'http://3.89.127.84:8080/Project2/rest/rec/'
  //private baseUrl: string = 'http://localhost:8080/Project2/rest/rec/'
   private getRecommendationsURL: string = 'all';  // URL to get all recommendations for a single user
   private getAllRecommendationsURL: string = 'entirelist';  // URL to get all recommendations
