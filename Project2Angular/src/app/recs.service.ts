a4a38d38ffeb823aeac7103a51e8f108f83a75d6
KaleHarrison <46387341+KaleHarrison@users.noreply.github.com>
2019-03-11 21:42:10 -0400

    Update recs.service.ts

--- Project2Angular/src/app/recs.service.ts
+++ Project2Angular/src/app/recs.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class RecsService {
 
- private baseUrl: string = "http://100.24.9.132:8080/Project2/rest/rec/submit";
+ private baseUrl: string = "http://54.92.162.19:8080/Project2/rest/rec/submit";
  //private baseUrl: string = "http://localhost:8080/Project2/rest/rec/submit";
 
   constructor(private http: HttpClient) { }
